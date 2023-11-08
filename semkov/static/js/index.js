cfc3ca68d70dedae6da1c2520537abfa3b23c1e2
Alexander Chaika <manti.by@gmail.com>
2023-11-08 23:49:58 +0300

    Fix contact form

--- semkov/static/js/index.js
+++ semkov/static/js/index.js
@@ -1,27 +1,47 @@
 (($) => {
 
-    'use strict';
+  "use strict";
 
-    $(document).ready(() => {
-        $.initHeader();
+  $.getCookie = (name, default_value) => {
+    let value = document.cookie, start = value.indexOf(" " + name + "="), end
 
-        $.initAjaxForms();
+    if (start === -1) start = value.indexOf(name + "=")
 
-        $('#contact-button').on('click', () => {
-            $('#contact-form').trigger('submit', (response) => {
-                $('#contact-modal').modal('hide');
-                alert(response['message']);
-            });
-        });
+    if (start === -1) {
+      value = default_value
+    } else {
+      start = value.indexOf("=", start) + 1
+      end = value.indexOf(";", start)
 
-        $('#login-button').on('click', () => {
-            $('#login-form').trigger('submit', (response) => {
-                $('#login-modal').modal('hide');
-                $('.login-alert').remove();
-                $('.login-button').addClass('d-none');
-                alert(response['message']);
-            });
-        });
-    });
+      if (end === -1) end = value.length
+      value = decodeURIComponent(value.substring(start, end))
+    }
 
-})(jQuery);
+    return value
+  }
+
+  $(document).ready(() => {
+    $(window).scroll(() => {
+      let header = $("header")
+      $(window).scrollTop() > 60 ? header.addClass("active") : header.removeClass("active")
+    })
+
+    $("#contact-button").on("click", (e) => {
+      e.preventDefault()
+
+      let form = $("#contact-form"), data = {"csrfmiddlewaretoken": $.getCookie("csrftoken")}
+
+      $.each(form.find(".form-control"), (i, input) => {
+        data[input.name] = input.value
+      })
+
+      $.post(form.attr("action"), data, (response) => {
+        $('#contact-modal').modal("hide")
+          alert(response["message"])
+      }).fail(() => {
+        alert("Что-то пошло не так, попробуй позже.")
+      })
+    })
+  })
+
+})(jQuery)
